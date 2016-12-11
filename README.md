49b848f3cd1e6a06fbe28f519e438a9f109fcedc
Daniel Zen <daniel@zen.digital>
2016-12-11 18:20:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## WebPack Example of React App
+
+This is the bootstrapped [Create React App](https://github.com/facebookincubator/create-react-app) project without the [react-script](https://github.com/facebookincubator/create-react-app/tree/master/packages/react-scripts).
+
+It has an explicit & highly simplified webpack.config.js so as to explain what 
+react-script is doing under the hood....
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname,'src/index.js')
+  ],
+  output: {
+    path: __dirname,
+    filename: 'app.js'
+  },
+  module: {
+    target: 'node',
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: { presets: ['es2015', 'react'] }
+    },
+    {
+      test: /\.css$/,
+      loader: 'style!css?importLoaders=1!postcss'
+    },
+    {
+      test: /\.(svg)$/,
+      loader: 'file',
+      query: {
+        name: 'static/media/[name].[hash:8].[ext]'
+      }
+    },
+    {
+      test: /\.(ico)$/,
+      loader: 'url',
+      query: {
+        name: 'static/media/[name].[hash:8].[ext]'
+      }
+    }]
+  },
+  plugins: [
+    new InterpolateHtmlPlugin({
+      PUBLIC_URL: 'http://localhost:8080/public'
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname,'public/index.html')
+    })
+  ]
+};
